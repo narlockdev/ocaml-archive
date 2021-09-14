46958e3cea9bc3696aa87c4aaf19caddd8cff6ad
Anthony <anthonynarlock@outlook.com>
2021-09-14 13:25:07 -0500

    Lab #2

---
+++ Laboratory Assignments/Lab_02/lab_02.mli
@@ -0,0 +1,14 @@
+(* This "interface" file indicates the types that your
+   functions in this lab must have. 
+ *)
+
+val power : int -> float -> float 
+
+val cube : float -> float 
+
+val circle_circum_v1 : float -> float
+
+val circle_circum_v2 : float -> float
+
+
+
