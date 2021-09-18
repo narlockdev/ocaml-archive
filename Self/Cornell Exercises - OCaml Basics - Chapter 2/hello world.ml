3d6f08d77cb6075cd2be3eec2e2da6729c4cee05
Anthony <anthonynarlock@outlook.com>
2021-09-18 09:10:13 -0500

    Create hello world.ml

---
+++ Self/Cornell Exercises - OCaml Basics - Chapter 2/hello world.ml
@@ -0,0 +1,15 @@
+(* hello world
+  Author: Anthony Narlock
+  Date: 9/18/2021
+*)
+
+(* Notice the differences between *)
+print_endline "Hello world!";;
+Hello world!
+- : unit = ()
+
+print_string "Hello world!";;
+Hello world!
+- : unit = ()
+
+(*print endline prints an end line at the end....*)
