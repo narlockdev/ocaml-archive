66b456211d8574f47ef7d3a2b104e1b684078424
narlock <anthonynarlock@outlook.com>
2021-11-17 18:46:08 -0600

    Taken

--- Self/Cornell Exercises - OCaml Basics - Chapter 2/if.ml
+++
@@ -1,9 +0,0 @@
-(* if
-  Author: Anthony Narlock
-  Date: 9/18/2021
-*)
-
-(* Write an if expression that evaluates to 42 if 2 is greater than 1
-  and otherwise evaluates to 7 *)
-if 2 > 1 then 42 else 7 ;;
-- : int = 42
